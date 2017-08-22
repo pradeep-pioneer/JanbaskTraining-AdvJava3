30489f090e285dba625563dd12f2aed1203beffb
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2017-08-22 12:47:31 +0530

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,29 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter output = response.getWriter();
+		output.write("<html><head><title>Welcome</title></head><body>");
+		output.write("<h1>Hello World from Janbask Training!</h1>");
+		output.write("</body></html>");
+	}
+
+}
